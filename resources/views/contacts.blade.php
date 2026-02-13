cd997b502e4e09c9090edbb8f1021b1fd8f30f1d
Kelvin-Cuinica <kelvincuinica@gmmail.com>
2026-02-13 23:38:45 +0200

    Criação de layouts usando Blade

--- resources/views/contacts.blade.php
+++ resources/views/contacts.blade.php
@@ -1,3 +1,8 @@
-<h1>Esta é a página de Contactos.</h1>
-<a href="/">Voltar ao Inicio</a>
-<a href="/produtos">Produtos</a>
+@extends('layouts.main')
+@section('title', 'Contactos')
+
+@section('content')
+    <h1>Esta é a página de Contactos.</h1>
+    <a href="/">Voltar ao Inicio</a>
+    <a href="/produtos">Produtos</a>
+@endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,62 +1,49 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.main')
+@section('title', 'HDC Events')
 
-        <title>Welcome</title>
+@section('content')
+    <h1>Ola, Mundo!😁</h1>
+    <a href="/contactos">Contactos</a>
+    <a href="/produtos">Produtos</a>
+    <a href="/login">Login</a>
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
-        <link rel="stylesheet" href="/css/styles.css">
-        <script src="/js/scripts.js"></script>
-    </head>
-    <body>
-    
-        <h1>Ola, Mundo!😁</h1>  
-        <a href="/contactos">Contactos</a>
-        <a href="/produtos">Produtos</a>
-        <a href="/login">Login</a>
 
-        
-        <div class="info">
-            <b>Nome: </b>{{ $nome }}<br>
-            <b>Apelido: </b>{{ $apelido }}
-        </div>
-        <div class="info-2">
-            @if($nome == "Pedro")
-                <p>O nome é Pedro.</p>
-            @elseif ($nome == "Kelvin")
-                <p>O nome é: {{ $nome }} {{ $apelido }}</p>
-                <img src="/img/banner.png" alt="Banner">
-            @else
-                <p>O nome não é Pedro.</p>
+    <div class="info">
+        <b>Nome: </b>{{ $nome }}<br>
+        <b>Apelido: </b>{{ $apelido }}
+    </div>
+    <div class="info-2">
+        @if ($nome == 'Pedro')
+            <p>O nome é Pedro.</p>
+        @elseif ($nome == 'Kelvin')
+            <p>O nome é: {{ $nome }} {{ $apelido }}</p>
+            <img src="/img/banner.png" alt="Banner">
+        @else
+            <p>O nome não é Pedro.</p>
+        @endif
+
+
+        @for ($i = 0; $i < count($arr); $i++)
+            <p>i = {{ $arr[$i] }} - indice {{ $i }}</p>
+            @if ($i == 2)
+                <p>* O i é 2</p>
             @endif
+        @endfor
 
 
-            @for($i=0; $i < count($arr); $i++)
-                <p>i = {{ $arr[$i] }} - indice {{ $i }}</p>
-                @if($i == 2)
-                    <p>* O i é 2</p>
-                @endif
-            @endfor
+        @php
+            $nome = 'Alberto';
+            $apelido = 'Mabunda';
+            $nomeCompleto = '<b>Nome:</b> ' . $nome . '<br>';
+            $nomeCompleto .= '<b>Apelido:</b> ' . $apelido;
+            echo $nomeCompleto;
+        @endphp
 
+        <!-- Comentario do HTML -->
+        {{-- Este é o comentário do Blade --}}
 
-           @php
-                $nome = "Alberto";
-                $apelido = "Mabunda";
-                $nomeCompleto = "<b>Nome:</b> " . $nome . "<br>";
-                $nomeCompleto .= "<b>Apelido:</b> " . $apelido;
-                echo $nomeCompleto;
-           @endphp
-
-            <!-- Comentario do HTML -->
-            {{-- Este é o comentário do Blade --}}
-
-            @foreach($nomes as $nome)
-                <p><b>Nome</b> {{ $loop->index }}: {{ $nome }} - {{ $profissao }}</p>
-            @endforeach
-        </div>
-    </body>
-</html>
+        @foreach ($nomes as $nome)
+            <p><b>Nome</b> {{ $loop->index }}: {{ $nome }} - {{ $profissao }}</p>
+        @endforeach
+    </div>
+@endsection
